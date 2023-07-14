6d382f095032d0f57a731146b9ebcbd0f33449a0
Yorick Downe <yorickdowne@gmail.com>
2023-07-14 19:13:25 +0000

    docusaurus

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -38,7 +38,7 @@ module.exports = {
         },
         algolia: {
             appId: '0ADJW6A3YH',
-            apiKey: 'a51366ded1891d634408de0e1e4cf1c5',
+            apiKey: '166b3180015f50f2daf044854e17fa9c',
             indexName: 'eth2-docker',
             searchParameters: { 'facetFilters': ["type:content", "version:current"] }
         }
